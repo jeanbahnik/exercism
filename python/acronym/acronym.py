a8cf4244f3ac898c212310cc073fbc87d6cdfd5a
Jean Bahnik <jeanbahnik@gmail.com>
2019-03-02 10:53:25 -0800

    Acronyms

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,9 @@
 def abbreviate(words):
-    pass
+    result = words[0]
+    separators = {' ', '-', '_'}
+
+    for i in range(len(words) - 1):
+        if words[i] in separators and words[i+1].isalpha():
+            result += words[i+1]
+
+    return result.upper()
