b36f275c30a6036e0437c56ec89c442d92c30d57
Jean Bahnik <jean.bahnik@rover.com>
2019-03-03 21:24:04 -0800

    Pangram done

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
 def is_pangram(sentence):
-    pass
+    LETTERS_IN_ALPHABET = 26
+    alphabet = []
+
+    for char in sentence:
+        if char.isalpha():
+            if char.lower() not in alphabet:
+                alphabet.append(char.lower())
+
+    if len(alphabet) == LETTERS_IN_ALPHABET:
+        return True
+    else:
+        return False
