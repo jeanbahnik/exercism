10226f30237d09183b82feca3edc7b155d900216
Jean Bahnik <jean.bahnik@rover.com>
2019-03-07 09:20:49 -0800

    High scores exercise

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,9 +1,4 @@
 def abbreviate(words):
-    result = words[0]
-    separators = {' ', '-', '_'}
-
-    for i in range(len(words) - 1):
-        if words[i] in separators and words[i+1].isalpha():
-            result += words[i+1]
-
-    return result.upper()
+    words = words.replace('-',' ').replace('_','')
+    characters = [word[0] for word in words.split()]
+    return "".join(characters).upper()
