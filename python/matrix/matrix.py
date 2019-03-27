9a9fa0e0ec98153809ec0e44739cb1230446cd14
Jean Bahnik <jean.bahnik@rover.com>
2019-03-27 13:00:35 -0700

    Matrix v1

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,19 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        rows = matrix_string.split('\n')
+        self.rows = []
+        for row in rows:
+            self.rows.append([int(n) for n in row.split()])
+        row_count = len(self.rows[0])
+        self.columns = []
+        for index in range(0, row_count):
+            column = []
+            for row in self.rows:
+                column.append(row[index])
+            self.columns.append(column)
 
     def row(self, index):
-        pass
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
+        return self.columns[index - 1]
