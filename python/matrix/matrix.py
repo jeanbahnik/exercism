5e74289b475fc8941ac9697839c2bb6ffc93f8b9
Jean Bahnik <jean.bahnik@rover.com>
2019-04-01 08:24:44 -0700

    Matrix refactor

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -5,15 +5,11 @@ class Matrix(object):
         for row in rows:
             self.rows.append([int(n) for n in row.split()])
 
-        self.columns = []
-        for index in range(0, len(self.rows[0])):
-            column = []
-            for row in self.rows:
-                column.append(row[index])
-            self.columns.append(column)
-
     def row(self, index):
         return self.rows[index - 1]
 
     def column(self, index):
-        return self.columns[index - 1]
+        columns = []
+        for row in self.rows:
+            columns.append(row[index - 1])
+        return columns
