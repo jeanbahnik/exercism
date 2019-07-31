e55f21c2df53d1c5aec659d5f8888570424ec292
Jean Bahnik <jeanbahnik@gmail.com>
2019-07-30 23:20:43 -0700

    code cleanup

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,12 +1,7 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
 export const value = (colors) => {
   let result = "";
   for (const color of colors) { result += COLORS.indexOf(color); }
   return parseInt(result);
 };
-
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
