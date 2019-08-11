ce60cfaa45043c41a48730b921cfc30413d03ddf
Jean Bahnik <jeanbahnik@gmail.com>
2019-08-10 18:38:32 -0700

    refactor resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,6 +2,6 @@ export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "bl
 
 export const value = (colors) => {
   let result = "";
-  for (const color of colors) { result += COLORS.indexOf(color); }
-  return parseInt(result);
+  colors = colors.map(color => result += COLORS.indexOf(color));
+  return +result;
 };
