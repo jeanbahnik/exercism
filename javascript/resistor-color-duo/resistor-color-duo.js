daaa98ea814baa1d5e8593eecc643f79c54beeb7
Jean Bahnik <jeanbahnik@gmail.com>
2019-08-11 11:15:37 -0700

    final resistor-color-duo

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,5 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
 export const value = (colors) => {
-  let result = "";
-  colors = colors.map(color => result += COLORS.indexOf(color));
-  return +result;
+  return Number(colors.map(color => COLORS.indexOf(color)).join(''));
 };
