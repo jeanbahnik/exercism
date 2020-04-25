bc641e35bc3b8b8e20b026e1783ad0efd02b2e8b
Jean Bahnik <jeanbahnik@gmail.com>
2020-04-25 15:47:46 -0700

    Acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
-
+class Acronym
+  def self.abbreviate(phrase)
+    phrase.split(/\s|-/).map{|c| c[0]}.join.upcase
+  end
+end
--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,5 +1,4 @@
-class ResistorColorDuo
-  COLORS = %w(black brown red orange yellow green blue violet grey white).freeze
+class ResistorColorDuo COLORS = %w(black brown red orange yellow green blue violet grey white).freeze
 
   def self.value(colors)
     colors.map{|color| COLORS.index(color)}.join[0..1].to_i
