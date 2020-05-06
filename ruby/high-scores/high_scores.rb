123b9bab658abcf78b612aca732eb6e5b555b4dd
Jean Bahnik <jeanbahnik@gmail.com>
2020-05-06 11:42:14 -0700

    Matrix solution 1

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,22 +1,20 @@
 class HighScores
+  attr_reader :scores
+
   def initialize(scores)
     @scores = scores
   end
 
-  def scores
-    @scores
-  end
-
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    @scores.max
+    scores.max
   end
 
   def personal_top_three
-    @scores.max(3)
+    scores.max(3)
   end
 
   def latest_is_personal_best?
