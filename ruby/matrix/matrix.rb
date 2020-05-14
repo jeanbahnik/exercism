a5fc8c9e3b2ef9dea5464a2da91dbd4d2b6c62b1
Jean Bahnik <jeanbahnik@gmail.com>
2020-05-14 11:29:01 -0700

    Matrix with improvements

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,25 @@
+# check the Array library to see if something can help you simplify the extract_columns code
+# check the String library, maybe there is something to deal with lines idiomatically?
+
+class Matrix
+attr_reader :rows, :columns
+
+  def initialize(matrix)
+    @rows = extract_rows(matrix)
+    @columns = extract_columns
+  end
+
+  def extract_rows(matrix)
+    matrix.split(/\n/).map! {|i| i.split.map!(&:to_i)}
+  end
+
+  def extract_columns
+    columns = Array.new(rows[0].length) {[]}
+
+    columns.each_with_index do |column, c_index|
+      rows.each_with_index do |row, r_index|
+        columns[c_index] << row[c_index]
+      end
+    end
+  end
+end
